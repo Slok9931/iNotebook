f000c221841dc856d0b7239f56c33c3205e1370e
Slok Tulsyan <148342312+Slok9931@users.noreply.github.com>
2024-11-05 13:31:29 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
   let navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`http://10.10.240.185:2000/api/auth/login`, {
+    const response = await fetch(`http://localhost:2000/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
